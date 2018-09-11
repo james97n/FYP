cb3c8ce6c90035df19ec5a9356e0de44553396f7
James Ng <30102700+jameszaii@users.noreply.github.com>
2018-09-12 01:57:53 +0800

    Initial commit

---
+++ app/src/main/java/com/james/fyp/MainActivity.java
@@ -0,0 +1,70 @@
+package com.james.fyp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public  class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_saved:
+                fragment = new SavedFragment();
+                break;
+
+            case R.id.navigation_history:
+                fragment = new HistoryFragment();
+                break;
+
+            case R.id.navigation_learning:
+                fragment = new LearningFragment();
+                break;
+
+
+        }
+        return loadFragment(fragment);
+
+
+    }
+
+}
